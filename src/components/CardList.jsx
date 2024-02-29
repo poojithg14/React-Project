203fc0d02f1d34fc5a9ae3a405c434c669eb0eac
poojithgavini <25764618-poojithgavini@users.noreply.replit.com>
2024-02-29 04:35:35 +0000

    Initial commit

---
+++ src/components/CardList.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import { BASE_URL } from '../config';
+
+import Card from './Card'
+import Button from './Button'
+import Search from './Search'
+
+const CardList = () => {
+  // define the limit state variable and set it to 10
+  const limit = 10;
+
+  // Define the offset state variable and set it to 0
+  const [offset, setOffset] = useState(0);
+  // Define the products state variable and set it to the default dataset
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = () => {
+    fetch(`${BASE_URL}/products?offset=${offset}&limit=${limit}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  }
+
+  // Define the handlePrevious function
+  const handlePrevious = () => {
+    // set the offset to the previous 10 products
+    setOffset(offset - limit);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, [offset]);
+
+  // Define the handleNext function
+  const handleNext = () => {
+    // set the offset to the next 10 products
+    setOffset(offset + limit);
+  }
+
+  const filterTags = (tag) => {
+    const filtered = data.filter(product => {
+      if (!tag) {
+        return product
+      }
+
+      return product.tags.find(({ title }) => title === tag)
+    })
+
+    setOffset(0)
+    setProducts(filtered)
+  }
+
+  const getPaginatedProducts = () => {
+    return products.slice(offset, offset + limit);
+  }
+
+  return (
+    <div className="cf pa2">
+      {/* The Search component is commented out, implement this with the API for extra credit */}
+      {/* <Search handleSearch={filterTags} /> */}
+      <div className="mt2 mb2">
+        {products && products.map((product) => (
+          <Card key={product._id} {...product} />
+        ))}
+      </div>
+
+      <div className="flex items-center justify-center pa4">
+        <Button text="Previous" handleClick={handlePrevious} />
+        <Button text="Next" handleClick={handleNext} />
+      </div>
+    </div>
+  )
+}
+
+export default CardList;
---
+++ src/components/SingleView.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import { BASE_URL } from '../config';
+import { useParams } from 'react-router-dom';
+import '../App.css';
+import AddToCart from './AddToCart'
+
+
+export default function SingleView() {
+  // get the id from the url using useParams
+  const { id } = useParams();
+
+  const [product, setProduct] = useState(null)
+
+  const fetchProductById = (id) => {
+    fetch(`${BASE_URL}/products/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProduct(data)
+      })
+  };
+
+  useEffect(() => {
+    fetchProductById(id)
+  }, [id]);
+
+  if (!product) return (<div className="loading-spinner"></div>);
+
+  const { user } = product;
+
+  const title = product.description ?? product.alt_description;
+  const style = {
+    backgroundImage: `url(${product.urls["regular"]})`
+  }
+
+  return (
+    <article className="bg-white center mw7 ba b--black-10 mv4">
+      <div className="pv2 ph3">
+        <div className="flex items-center">
+          <img src={user?.profile_image?.medium} className="br-100 h3 w3 dib" alt={user.instagram_username} />
+          <h1 className="ml3 f4">{user.first_name} {user.last_name}</h1>
+        </div>
+      </div>
+      <div className="aspect-ratio aspect-ratio--4x3">
+        <div className="aspect-ratio--object cover" style={style}></div>
+      </div>
+      <div className="pa3 flex justify-between">
+        <div className="mw6">
+          <h1 className="f6 ttu tracked">Product ID: {id}</h1>
+          <a href={`/products/${id}`} className="link dim lh-title">{title}</a>
+        </div>
+        <div className="gray db pv2">&hearts;<span>{product.likes}</span></div>
+      </div>
+      <div className="pa3 flex justify-end">
+        <span className="ma2 f4">${product.price}</span>
+        <AddToCart product={product} />
+      </div>
+    </article>
+
+  )
+}
